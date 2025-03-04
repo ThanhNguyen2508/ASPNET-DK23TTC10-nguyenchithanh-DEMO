cf91a07e62ca9fd93ca7e8006e637cfa8a9f83b7
nguyenchithanh <kingoftheworld2508@gmail.com>
2025-03-04 09:15:57 +0700

    database

---
+++ websitecafe/Models/DBWebcafeContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace websitecafe.Models
+{
+    public class DBWebcafeContext : DbContext
+    {
+        public DBWebcafeContext() : base("DBConnectionString") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
