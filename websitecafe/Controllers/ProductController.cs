e4e20ad890ab9a2bcd54f364cedd32d749240b25
nguyenchithanh <kingoftheworld2508@gmail.com>
2025-03-05 09:42:55 +0700

    update code

--- websitecafe/Controllers/ProductController.cs
+++ websitecafe/Controllers/ProductController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using websitecafe.DAO;
 
 namespace websitecafe.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
-        public ActionResult Index()
+        private readonly ProductDao _productDao;
+
+        public ProductController()
         {
-            return View();
+            _productDao = new ProductDao();
+        }
+
+        public ActionResult Index(int page = 1, int pageSize = 6, int? categoryId = null, string search = "")
+        {
+            int totalProducts;
+            var products = _productDao.GetProductsByPage(page, pageSize, out totalProducts, categoryId, search);
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)System.Math.Ceiling((double)totalProducts / pageSize);
+            ViewBag.Search = search;
+            ViewBag.CategoryId = categoryId;
+            ViewBag.Categories = _productDao.GetAllCategories(); // Lấy danh sách danh mục
+
+            return View(products);
         }
     }
 }
--- websitecafe/Daos/ProductDao.cs
+++ websitecafe/Daos/ProductDao.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using websitecafe.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Windows.Forms;
 
 namespace websitecafe.DAO
 {
@@ -10,19 +9,64 @@ namespace websitecafe.DAO
     {
         private readonly DBWebcafeContext _context;
 
-        public ProductDao(DBWebcafeContext context)
+        public ProductDao()
         {
-            _context = context;
+            _context = new DBWebcafeContext();
         }
 
-        public List<Product> GetAllProducts()
+        public List<Product> GetAllProducts(int pageNumber, int pageSize)
         {
-            return _context.Products.Include("Category").ToList();
+            return _context.Products
+                           .Include("Category") // Sử dụng nameof tránh lỗi refactoring
+                           .OrderBy(p => p.Id)
+                           .Skip((pageNumber - 1) * pageSize)
+                           .Take(pageSize)
+                           .ToList();
+        }
+
+        public List<Product> GetProductsByPage(int pageNumber, int pageSize, out int totalProducts, int? categoryId, string search)
+        {
+            var query = _context.Products.Include("Category").AsQueryable();
+
+            // Lọc theo danh mục (nếu có)
+            if (categoryId.HasValue)
+            {
+                query = query.Where(p => p.CategoryId == categoryId);
+            }
+
+            // Tìm kiếm theo tên sản phẩm
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(p => p.Name.Contains(search));
+            }
+
+            totalProducts = query.Count();
+
+            return query.OrderBy(p => p.Id)
+                        .Skip((pageNumber - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToList();
+        }
+
+        public int GetTotalProductsCount()
+        {
+            return _context.Products.Count();
+        }
+
+        public List<Product> GetTopViewedProducts(int top = 8)
+        {
+            return _context.Products
+                           .Include("Category")
+                           .OrderByDescending(p => p.View)
+                           .Take(top)
+                           .ToList();
         }
 
         public Product GetProductById(int id)
         {
-            return _context.Products.Include("Category").FirstOrDefault(p => p.Id == id);
+            return _context.Products
+                           .Include("Category")
+                           .FirstOrDefault(p => p.Id == id);
         }
 
         public void AddProduct(Product product)
@@ -31,28 +75,36 @@ namespace websitecafe.DAO
             _context.SaveChanges();
         }
 
-        public void UpdateProduct(Product product)
+        public bool UpdateProduct(Product product)
         {
             var existingProduct = _context.Products.Find(product.Id);
-            if (existingProduct != null)
-            {
-                existingProduct.Name = product.Name;
-                existingProduct.Price = product.Price;
-                existingProduct.Description = product.Description;
-                existingProduct.ImageUrl = product.ImageUrl;
-                existingProduct.CategoryId = product.CategoryId;
-                _context.SaveChanges();
-            }
+            if (existingProduct == null)
+                return false;
+
+            existingProduct.Name = product.Name;
+            existingProduct.Price = product.Price;
+            existingProduct.Description = product.Description;
+            existingProduct.ImageUrl = product.ImageUrl;
+            existingProduct.CategoryId = product.CategoryId;
+
+            _context.SaveChanges();
+            return true;
         }
 
-        public void DeleteProduct(int id)
+        public bool DeleteProduct(int id)
         {
             var product = _context.Products.Find(id);
-            if (product != null)
-            {
-                _context.Products.Remove(product);
-                _context.SaveChanges();
-            }
+            if (product == null)
+                return false;
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _context.Categories.ToList();
         }
     }
 }
